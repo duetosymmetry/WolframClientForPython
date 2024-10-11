730d9d7607faf0b99061d990a481beaaf47495d4
Riccardo Di Virgilio <riccardod@wolfram.com>
2024-10-11 13:05:14 +0200

    avoid doing first, we have no idea how the batching works, just serialize all of them.

--- wolframclient/serializers/encoders/pyarrow.py
+++ wolframclient/serializers/encoders/pyarrow.py
@@ -2,25 +2,31 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from wolframclient.utils.api import pyarrow
 from wolframclient.utils.dispatch import Dispatch
-from wolframclient.utils.functional import first
 
 encoder = Dispatch()
 
 
-@encoder.dispatch(pyarrow.RecordBatch)
-def encoder_pyarrow_table(serializer, batch):
-    sink = pyarrow.BufferOutputStream()
-    strm = pyarrow.ipc.new_stream(sink, batch.schema)
-    strm.write_batch(batch)
-    buf = sink.getvalue()
+def _encode_batches(serializer, batches, schema):
+    buffer = pyarrow.BufferOutputStream()
+    stream = pyarrow.ipc.new_stream(buffer, schema)
+    for batch in batches or (
+        pyarrow.RecordBatch.from_arrays([[] for _ in schema], schema=schema),
+    ):
+        stream.write_batch(batch)
     return serializer.serialize_function(
         serializer.serialize_symbol(b"ImportByteArray"),
-        (serializer.serialize_bytes(buf), serializer.serialize_string("ArrowIPC")),
+        (
+            serializer.serialize_bytes(buffer.getvalue()),
+            serializer.serialize_string("ArrowIPC"),
+        ),
     )
+
+
+@encoder.dispatch(pyarrow.RecordBatch)
+def encoder_pyarrow_batch(serializer, batch):
+    return _encode_batches(serializer, (batch,), batch.schema)
 
 
 @encoder.dispatch(pyarrow.Table)
-def encoder_pyarrow_table(serializer, o):
-    return serializer.encode(
-        first(o.to_batches(max_chunksize=1)) or pyarrow.RecordBatch.from_arrays([[] for _ in o.schema], schema=o.schema)
-    )
+def encoder_pyarrow_table(serializer, table):
+    return _encode_batches(serializer, table.to_batches(), table.schema)
