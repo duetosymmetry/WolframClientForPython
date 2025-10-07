f45b822f7ce8494c052c5ccce1db54bdd42d19a9
Riccardo Di Virgilio <riccardod@wolfram.com>
2025-10-07 17:44:27 +0200

    code lint

--- wolframclient/serializers/encoders/pandas.py
+++ wolframclient/serializers/encoders/pandas.py
@@ -11,16 +11,19 @@ from wolframclient.utils.datastructures import Settings
 
 encoder = Dispatch()
 
+
 def arrow_to_association(o):
     return wl.AssociationThread(wl.Range(0, len(o) - 1), wl.Normal(o))
 
+
 encoders = dict(
-    tabular = identity,
-    association = composition(arrow_to_association),
-    dataset = composition(arrow_to_association, wl.Dataset),
-    list = composition(arrow_to_association, wl.Normal),
-    values = composition(wl.Normal),
+    tabular=identity,
+    association=composition(arrow_to_association),
+    dataset=composition(arrow_to_association, wl.Dataset),
+    list=composition(arrow_to_association, wl.Normal),
+    values=composition(wl.Normal),
 )
+
 
 def internal_serialize(serializer, o, prop_name, default, preprocessor):
 
@@ -29,29 +32,43 @@ def internal_serialize(serializer, o, prop_name, default, preprocessor):
     if not head in encoders:
         raise ValueError(
             "Invalid value for property '{}'. Expecting one of ({}), got {}.".format(
-                prop_name,
-                ", ".join(encoders.keys()), head
+                prop_name, ", ".join(encoders.keys()), head
             )
         )
 
-    func = composition(
-        preprocessor,
-        encoders[head],
-        serializer.encode
-    )
+    func = composition(preprocessor, encoders[head], serializer.encode)
 
     return func(o)
 
 
 @encoder.dispatch(pandas.DataFrame)
 def encoder_panda_dataframe(serializer, o):
-    return internal_serialize(serializer, o, prop_name = "pandas_dataframe_head", default = 'dataset', preprocessor = pyarrow.record_batch)
+    return internal_serialize(
+        serializer,
+        o,
+        prop_name="pandas_dataframe_head",
+        default="dataset",
+        preprocessor=pyarrow.record_batch,
+    )
+
 
 @encoder.dispatch(pandas.DatetimeIndex)
 def encoder_panda_dataframe(serializer, o):
-    return internal_serialize(serializer, o, prop_name = "pandas_dataframe_head", default = 'values', preprocessor = pyarrow.array)
+    return internal_serialize(
+        serializer,
+        o,
+        prop_name="pandas_dataframe_head",
+        default="values",
+        preprocessor=pyarrow.array,
+    )
+
 
 @encoder.dispatch(pandas.Series)
 def encode_panda_series(serializer, o):
-    return internal_serialize(serializer, o, prop_name = "pandas_dataframe_head", default = 'association', preprocessor = pyarrow.array)
-
+    return internal_serialize(
+        serializer,
+        o,
+        prop_name="pandas_dataframe_head",
+        default="association",
+        preprocessor=pyarrow.array,
+    )
