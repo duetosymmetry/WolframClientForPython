efbe2db9704b7cb840fff9e917e539ee60e5a3c5
Riccardo Di Virgilio <riccardod@wolfram.com>
2025-12-09 15:40:44 +0100

    use to dense

--- wolframclient/serializers/encoders/pandas.py
+++ wolframclient/serializers/encoders/pandas.py
@@ -3,16 +3,16 @@ from __future__ import absolute_import, print_function, unicode_literals
 from collections import OrderedDict
 from itertools import starmap
 
-from wolframclient.utils.api import pandas, pyarrow
-from wolframclient.utils.dispatch import Dispatch
 from wolframclient.language import wl
-from wolframclient.utils.functional import identity, composition
+from wolframclient.utils.api import pandas, pyarrow
 from wolframclient.utils.datastructures import Settings
+from wolframclient.utils.dispatch import Dispatch
+from wolframclient.utils.functional import composition, identity
 
 encoder = Dispatch()
 
 
-def arrow_to_association(o, index = None):
+def arrow_to_association(o, index=None):
     return wl.AssociationThread(index or wl.Range(0, len(o) - 1), wl.Normal(o))
 
 
@@ -26,7 +26,6 @@ encoders = dict(
 
 
 def internal_serialize(serializer, o, index, prop_name, default):
-
     head = serializer.get_property(prop_name, d=None) or default
 
     if not head in encoders:
@@ -45,7 +44,7 @@ def internal_serialize(serializer, o, index, prop_name, default):
 def encoder_panda_dataframe(serializer, o):
     return internal_serialize(
         serializer,
-        pyarrow.RecordBatch.from_pandas(o, preserve_index = False),
+        pyarrow.RecordBatch.from_pandas(o, preserve_index=False),
         index=o.index.tolist(),
         prop_name="pandas_dataframe_head",
         default="dataset",
@@ -65,6 +64,9 @@ def encoder_panda_dataframe(serializer, o):
 
 @encoder.dispatch(pandas.Series)
 def encode_panda_series(serializer, o):
+    if hasattr(o, "sparse"):
+        o = o.sparse.to_dense()
+
     return internal_serialize(
         serializer,
         pyarrow.Array.from_pandas(o),
