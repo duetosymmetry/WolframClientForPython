a089cdd4f059a959f7fb7dd997c1e9b2c4936433
Riccardo Di Virgilio <riccardod@wolfram.com>
2025-12-23 15:03:53 +0100

    working implementation

--- wolframclient/serializers/encoders/pandas.py
+++ wolframclient/serializers/encoders/pandas.py
@@ -24,29 +24,83 @@ def is_auto_index(index):
 
 
 
-
-def arrow_to_association(o, index):
-    return wl.AssociationThread(index.to_list(), wl.Normal(o))
-
-
-encoders = dict(
-    tabular=lambda o, i: o,
-    values=lambda o, i: wl.Normal(o),
-    association=arrow_to_association,
-    dataset=composition(arrow_to_association, wl.Dataset),
-    list=composition(arrow_to_association, wl.Normal),
-)
-
-
-def internal_serialize(serializer, o):
-
-    head = is_legacy_mode() and 'legacy' or 'tabular'
+def _distribute_multikey(o):
+    """Distribute MultiIndex keys into nested OrderedDicts."""
+    expr_dict = OrderedDict()
+    for multikey, value in o.items():
+        cur_dict = expr_dict
+        for key in multikey[:-1]:
+            if key not in cur_dict:
+                cur_dict[key] = OrderedDict()
+            cur_dict = cur_dict[key]
+        cur_dict[multikey[-1]] = value
+    return expr_dict
 
 
+def legacy_encode_series(serializer, series):
+    """Encode a Series based on its index type.
+
+    - DatetimeIndex -> TimeSeries
+    - MultiIndex -> nested associations
+    - Regular index -> association
+    """
+    length = len(series)
+
+    if isinstance(series.index, pandas.DatetimeIndex):
+        # Encode as TimeSeries
+        return serializer.serialize_function(
+            serializer.serialize_symbol(b"TimeSeries"),
+            (
+                serializer.serialize_iterable(
+                    (
+                        serializer.serialize_iterable(
+                            (serializer.encode(idx), serializer.encode(val)), length=2
+                        )
+                        for idx, val in series.items()
+                    ),
+                    length=length,
+                ),
+            ),
+        )
+    elif isinstance(series.index, pandas.MultiIndex):
+        # Encode as nested associations
+        return serializer.encode(_distribute_multikey(series))
+    else:
+        # Encode as simple association
+        return serializer.serialize_association(
+                (
+                    (serializer.encode(idx), serializer.encode(val))
+                    for idx, val in series.items()
+                ),
+                length=length,
+            )
+
+def dataset(serializer, *args):
+    return serializer.serialize_function(
+        serializer.serialize_symbol(b"Dataset"),
+        args,
+    )
+
+def legacy_encode(serializer, o):
+    """Encode DataFrame as Dataset[<association of row -> association of col -> value>].
+
+    This replicates the old default behavior before the tabular implementation.
+    The old code used o.T.items() to iterate over rows (transposed columns become rows).
+    """
+    return dataset(serializer, serializer.serialize_association(
+            (
+                (serializer.encode(k), legacy_encode_series(serializer, v))
+                for k, v in o.T.items()
+            ),
+            length=len(o.index),
+        ))
+    
+
+
+def pyarrow_serialize(serializer, o):
     if is_auto_index(o.columns):
         new_columns = [column_formatter(i) for i in range(len(o.columns))]
         o = o.set_axis(new_columns, axis=1)
-
 
     if not is_auto_index(o.index):
         new_names = [
@@ -54,29 +108,23 @@ def internal_serialize(serializer, o):
         ]
         o.index = o.index.set_names(new_names)
 
-    func = composition(encoders[head], serializer.encode)
-
-    return func(
-        pyarrow.RecordBatch.from_pandas(
-            o, preserve_index=not is_auto_index(o.index) and head == "tabular"
-        ),
-        o.index,
+    return serializer.encode(
+        pyarrow.RecordBatch.from_pandas(o, preserve_index=not is_auto_index(o.index))
     )
 
 
 @encoder.dispatch(pandas.DataFrame)
 def encoder_panda_dataframe(serializer, o):
-    return internal_serialize(
-        serializer, o
-    )
+    if is_legacy_mode():
+        return legacy_encode(serializer, o)
+    return pyarrow_serialize(serializer, o)
 
 
 @encoder.dispatch(pandas.DatetimeIndex)
-def encoder_panda_dataframe(serializer, o):
-    return internal_serialize(
-        serializer,
-        o.to_frame(),
-    )
+def encoder_panda_datetimeindex(serializer, o):
+    if is_legacy_mode():
+        return legacy_encode_series(serializer, o.to_series())
+    return pyarrow_serialize(serializer, o.to_frame())
 
 
 @encoder.dispatch(pandas.Series)
@@ -84,7 +132,6 @@ def encode_panda_series(serializer, o):
     if hasattr(o, "sparse"):
         o = o.sparse.to_dense()
 
-    return internal_serialize(
-        serializer,
-        o.to_frame(),
-    )
+    if is_legacy_mode():
+        return legacy_encode_series(serializer, o)
+    return pyarrow_serialize(serializer, o.to_frame())
